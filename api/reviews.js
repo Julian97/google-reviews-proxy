5eccce1502d89a4ea0858c601b8b23f01986355b
Julian Cheung <juliancheung97@gmail.com>
2025-08-12 23:09:37 +0800

    Update reviews.js

--- api/reviews.js
+++ api/reviews.js
@@ -1,7 +1,3 @@
 export default function handler(req, res) {
-  res.status(200).json({
-    message: "API is running",
-    apiKey: process.env.GOOGLE_API_KEY ? "key present" : "key missing",
-    placeId: process.env.PLACE_ID ? "place id present" : "place id missing"
-  });
+  res.status(200).json({ msg: "This is the NEW deployment" });
 }
