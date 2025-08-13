43315c62ec83ab35140dda60acdafe95f43f70d5
Julian Cheung <juliancheung97@gmail.com>
2025-08-13 11:40:10 +0800

    Delete api/reviewsdebug.js

--- api/reviewsdebug.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    message: "API is running",
-    apiKey: process.env.GOOGLE_API_KEY ? "key present" : "key missing",
-    placeId: process.env.PLACE_ID ? "place id present" : "place id missing"
-  });
-}
